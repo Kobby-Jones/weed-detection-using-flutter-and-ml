78444db3e79faabfe3002a65e04620f43770abdd
Kobby Jones <kobbyj099@gmail.com>
2024-07-03 21:50:06 +0000

    Home Page

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -33,13 +33,50 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-         
-        body: const Center(
+        //  The Body Start here
+        // Position all the components in the center of the page
+        body: Center(
+          // Arrange the components in a column order
           child: Column(
             children: <Widget>[
-              SizedBox(height: 70.0,),
-              Image(image: AssetImage("images/weedradar-logo.png"), width: 160.0, height: 160.0,),
-              
+              // Givesome space between the appbar and the logo
+              const SizedBox(height: 70.0,),
+              // The App Log
+              const Image(image: AssetImage("images/weedradar-logo.png"), width: 160.0, height: 160.0,),
+              // The home Title
+              const Text("WEEDRADAR", style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold,)),
+              // A card to give a brief info about the app
+              const Card(child: Padding(
+                padding: EdgeInsets.only(right: 50.0, left: 10.0, top: 20, bottom: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Real-Time Weed Detect", style: TextStyle(fontSize:24.0, fontWeight: FontWeight.bold),),
+                    Text("Track and Manage Weed", style: TextStyle(fontSize:15.0, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 20.0,),
+                    Icon(Icons.camera_alt_outlined, color: Colors.black,)
+                  ],
+                ),
+              ),),
+              const SizedBox(height: 20.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Detect Button
+                ElevatedButton(onPressed: (){}, style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color(0xFF187F5A))), child: const Text("Detect", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.white),)),
+                const SizedBox(width: 40.0,),
+                // Manage Button 
+                ElevatedButton(onPressed: (){}, style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color(0xFF187F5A))), child: const Text("Manage",  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.white),)),
+              ],),
+              const SizedBox(height: 20.0,),
+               const Padding(
+                 padding: EdgeInsets.symmetric(horizontal: 20.0),
+                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                   CircleAvatar(backgroundColor: Colors.black,child: Icon(Icons.question_mark_rounded, color: Colors.white,),)
+                               ],),
+               )
             ],
           ),
         ),
