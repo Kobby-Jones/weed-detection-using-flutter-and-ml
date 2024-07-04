c0c4dab1a551651e26ca726a2d869871ac885a3b
Kobby Jones <kobbyj099@gmail.com>
2024-07-04 03:24:31 +0000

    Onboard Welcome Screen

---
+++ lib/Pages/onboard_welcome_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Column(
+          children: <Widget>[
+            
+          ],
+        ),),
+      ),
+    );
+  }
+}
